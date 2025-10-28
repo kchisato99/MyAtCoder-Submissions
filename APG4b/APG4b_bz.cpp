5d2934642594f291c094299c3ca332f00216d6da
kchisato99 <kchisato99@gmail.com>
2025-10-28 14:51:29 +0000

    Added my codes from AtCoder

---
+++ APG4b/APG4b_bz.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int N;
+  cin >> N;
+
+  vector<int> a(N);
+  for (int i = 0; i < N; i++)
+  {
+    cin >> a[i];
+  }
+
+  map<int, int> countNum; // 値と出現回数の組
+  int maxNum = 0;         // 最大出現回数の値
+  for (int i = 0; i < N; i++)
+  {
+    countNum[a[i]]++;
+    if (countNum[a[i]] > countNum[maxNum])
+    {
+      maxNum = a[i];
+    }
+  }
+
+  cout << maxNum << " " << countNum[maxNum] << endl;
+}
